acc71cc531fee29b2d435ae395d0bfcc3b3a2175
Stamat <xsouvd2@gmail.com>
2025-02-13 11:23:52 +0200

    added a mini program

--- git-testing/Program.cs
+++ git-testing/Program.cs
@@ -4,7 +4,10 @@
     {
         static void Main(string[] args)
         {
-            
+            int a = int.Parse(Console.ReadLine());
+            int b = int.Parse(Console.ReadLine());
+
+            Console.WriteLine(a+b);
         }
     }
 }
